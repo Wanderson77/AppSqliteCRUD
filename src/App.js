542047aa3d4a2f569ffe340e08f67de13d7a61ec
Wanderson Aldo <wanderson.aldo@gmail.com>
2020-01-29 08:42:58 -0200

    Commit base

---
+++ src/App.js
@@ -0,0 +1,72 @@
+/*Example of SQLite Database in React Native*/
+import React from 'react';
+//For react-navigation 3.0+
+//import { createAppContainer, createStackNavigator } from 'react-navigation';
+//For react-navigation 4.0+
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator} from 'react-navigation-stack';
+
+import HomeScreen from './pages/HomeScreen';
+import RegisterUser from './screens/RegistrarUsuario';
+import UpdateUser from './screens/AtualizarUsuario';
+import ViewUser from './screens/VisualizarUsuario';
+import ViewAllUser from './screens/VisualizarTodoUsuario';
+import DeleteUser from './screens/DeletarUsuario';
+
+const App = createStackNavigator({
+  
+  HomeScreen: {
+    screen: HomeScreen,
+    navigationOptions: {
+      title: 'HomeScreen',
+      headerStyle: { backgroundColor: '#f05555' },
+      headerTintColor: '#ffffff',
+    },
+  },
+
+  View: {
+    screen: ViewUser,
+    navigationOptions: {
+      title: 'View User',
+      headerStyle: { backgroundColor: '#f05555' },
+      headerTintColor: '#ffffff',
+    },
+  },
+
+  ViewAll: {
+    screen: ViewAllUser,
+    navigationOptions: {
+      title: 'View All User',
+      headerStyle: { backgroundColor: '#f05555' },
+      headerTintColor: '#ffffff',
+    },
+  },
+
+  Update: {
+    screen: UpdateUser,
+    navigationOptions: {
+      title: 'Update User',
+      headerStyle: { backgroundColor: '#f05555' },
+      headerTintColor: '#ffffff',
+    },
+  },
+
+  Register: {
+    screen: RegisterUser,
+    navigationOptions: {
+      title: 'Register User',
+      headerStyle: { backgroundColor: '#f05555' },
+      headerTintColor: '#ffffff',
+    },
+  },
+
+  Delete: {
+    screen: DeleteUser,
+    navigationOptions: {
+      title: 'Delete User',
+      headerStyle: { backgroundColor: '#f05555' },
+      headerTintColor: '#ffffff',
+    },
+  },
+});
+export default createAppContainer(App);
